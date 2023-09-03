a3c2c87ed4890ea1d55eeede79a2fe99d6d008be
JahirA4 <jahiruddin.butex@gmail.com>
2023-09-02 21:43:49 -0400

    card ready

--- ai.js
+++ ai.js
@@ -6,59 +6,52 @@ const laodTools = async () => {
 
 }
 
-// generate dynamic url
-
-const displayTools = (tools) => {
-
-
-    tools.forEach(tool => {
-        const url = `https://openapi.programming-hero.com/api/ai/tool/${tool.id}`
-        fetch(url)
-            .then(res => res.json())
-            .then(data => displayAi(data))
-    });
-
-
-}
-
 // display Ai tools in cards
 
-const displayAi = (technology) => {
+const displayTools = (technologies) => {
     const techContainer = document.getElementById('tech-container')
-    // console.log(technology.data.features)
-    const techDiv = document.createElement('div')
-    techDiv.classList.add('col')
-
-    // array of objects of the features
-    const valuesFeature=Object.values(technology.data.features)
-    // array of features
-    const features=valuesFeature.map(feature=>feature.feature_name)
-
-    // console.log(valuesFeature)
-    console.log(features)
-
-
-
-    techDiv.innerHTML = `
+    console.log(technologies)  
+    technologies.forEach(technology => {
+        const techDiv = document.createElement('div')
+        techDiv.classList.add('col')
+        techDiv.innerHTML = `
         <div class="card h-100">
-          <img src="${technology.data.image_link[0]}" class="card-img-top" alt="...">
+          <img src="${technology.image}" class="card-img-top" alt="...">
           <div class="card-body">
               <h5 class="card-title">Features</h5> 
           </div>
           
-        <div>
-        <ol>
-          ${features.map((item, i) => `<li>${item}</li>`).join('')}
-        </ol>   
-        </div>
-            <div class="card-footer">
-              <small class="text-body-secondary">Last updated 3 mins ago</small>
+            <div>
+                <ol>
+                 ${technology.features.map((item, i) => `<li>${item}</li>`).join('')}
+                </ol>   
+            </div>
+            <div class="card-footer p-5">
+               
+                <h4>${technology.name}</h4>
+                
+                
+
+                <div class="position-relative mb-3">
+                    <div class="position-absolute top-0 start-0">
+                        <i class="fa-regular fa-calendar">  ${technology.published_in}</i>
+                    </div>
+                    <div class="position-absolute top-0 end-0">
+                        <button type="button" class="btn btn-light btn-lg float-right"> <i class="fa-solid fa-arrow-right" style="color: #fb1818;"></i></button>
+                    </div>
+                </div>
+                
+                
+                
+                  
             </div>
         </div>
       `
-    techContainer.appendChild(techDiv)
+      techContainer.appendChild(techDiv)
 
-
+    });
+   
+    
 
 }
 
