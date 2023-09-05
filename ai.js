31187cd0fd43f002ad1073bca682aa58839a4392
JahirA4 <jahiruddin.butex@gmail.com>
2023-09-04 22:00:04 -0400

    modal features added

--- ai.js
+++ ai.js
@@ -10,9 +10,10 @@ const laodTools = async () => {
 
 const displayTools = (technologies) => {
     const techContainer = document.getElementById('tech-container')
-    console.log(technologies)  
+    // console.log(technologies)  
     technologies.forEach(technology => {
         const techDiv = document.createElement('div')
+        // console.log(technology.id)
         techDiv.classList.add('col')
         techDiv.innerHTML = `
         <div class="card h-100">
@@ -37,13 +38,11 @@ const displayTools = (technologies) => {
                         <i class="fa-regular fa-calendar">  ${technology.published_in}</i>
                     </div>
                     <div class="position-absolute top-0 end-0">
-                        <button type="button" class="btn btn-light btn-lg float-right"> <i class="fa-solid fa-arrow-right" style="color: #fb1818;"></i></button>
+                        <button onclick="loadToolDetails('${technology.id}')" type="button" class="btn btn-light btn-lg float-right" data-bs-toggle="modal" data-bs-target="#toolDetailModal"> <i class="fa-solid fa-arrow-right" style="color: #fb1818;"></i></button>
                     </div>
                 </div>
                 
-                
-                
-                  
+                                  
             </div>
         </div>
       `
@@ -51,9 +50,36 @@ const displayTools = (technologies) => {
 
     });
    
-    
+ 
 
 }
 
+const loadToolDetails=async id=>{
+   const url=`https://openapi.programming-hero.com/api/ai/tool/${id}`
+   const res=await fetch(url);
+   const data=await res.json();
+   openModal(data);
+   
+}
+
+const openModal=tool=>{
+    console.log(tool.data.integrations)
+    const toolDescription=document.getElementById("description")
+    // object of objects of features
+    toolDescription.innerText=tool.data.description
+    // array of object values
+    const cardFeatures=Object.values(tool.data.features)
+    // features added in card
+    const featureDiv=document.getElementById('card-features')
+    featureDiv.innerHTML=`
+    <h4>Features</h4>
+    ${cardFeatures.map(item=>`
+    
+    <li>${item.feature_name}</li>
+    `).join("")}`
+
+
+
+}
 
 laodTools()
